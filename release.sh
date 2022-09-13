3e698bed997c52fdd5d6499e52ec682b9985bff2
alex.oleshkevich <alex.oleshkevich@gmail.com>
2022-09-13 12:52:52 +0300

    fix release script

--- release.sh
+++ release.sh
@@ -7,8 +7,8 @@ UPSTREAM_VERSION=$(grep -o  -P '(?<=icons-).*(?=.tgz)' yarn.lock)
 if git status | grep yarn.lock; then
     echo "Icons updated to $UPSTREAM_VERSION"
 else
-    exit
     echo 'Icons up to date.'
+    exit
 fi
 
 # drop any unwanted changes to package.json
@@ -16,7 +16,7 @@ git checkout -- package.json
 
 yarn version --minor
 PACKAGE_VERSION=$(grep -o  -P '(?<=version": ").*(?=")' package.json)
-MESSAGE="Release $PACKAGE_VERSION (sync with @tabler/icons@$UPSTREAM_VERSION)"
+MESSAGE="Release $PACKAGE_VERSION (sync with @tabler/icons = $UPSTREAM_VERSION)"
 
 git add -A
 git commit -a -m "$MESSAGE"
